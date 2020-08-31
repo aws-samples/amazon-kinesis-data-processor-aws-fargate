751b660d2ba7504b012bfaa1d62f6f94415efdee
Florian Mair <flomair@amazon.com>
2020-08-31 22:18:32 +0200

    Automatic AWS Region Configuration & Consumer Logger

--- consumer_v2/run.sh
+++ consumer_v2/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 `python3 /usr/bin/amazon_kclpy_helper.py --print_command --java /usr/bin/java --properties app/record_processor.properties --log-configuration app/logback.xml` &
 sleep 10
-touch /app/record_processor.log
-exec tail -f /app/record_processor.log
+touch /app/logs/record_processor.log
+exec tail -f /app/logs/record_processor.log
