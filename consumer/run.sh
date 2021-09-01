a74a93084df6abb7887b5ac1b25607d381b3c6a1
Daniel Lorch <dlorch@gmail.com>
2021-09-01 21:22:39 +0200

    Fix - reopen file when it is rotated away

--- consumer/run.sh
+++ consumer/run.sh
@@ -3,4 +3,4 @@ python3 /usr/bin/set_properties.py
 chmod 777 /usr/bin/record_processor.py
 `python3 /usr/bin/amazon_kclpy_helper.py --print_command --java /usr/bin/java --properties /usr/bin/record_processor.properties --log-configuration /usr/bin/logback.xml` &
 touch /app/logs/record_processor.log
-exec tail -f /app/logs/record_processor.log
+exec tail -F /app/logs/record_processor.log
