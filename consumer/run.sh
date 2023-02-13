147ca29becb12788774c25559f56bc896c070664
Florian Mair <flomair@amazon.com>
2023-02-13 13:34:04 +0100

    Change consumer version

--- consumer/run.sh
+++ consumer/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3 /usr/bin/set_properties.py
 chmod 777 /usr/bin/record_processor.py
-`python3 /usr/bin/amazon_kclpy_helper.py --print_command --java /usr/bin/java --properties /usr/bin/record_processor.properties --log-configuration /usr/bin/logback.xml` &
+`python3 /usr/bin/amazon_kclpy_helper.py --print_command --java /usr/bin/java --properties /usr/bin/sample.properties --log-configuration /usr/bin/logback.xml` &
 touch /app/logs/record_processor.log
 exec tail -F /app/logs/record_processor.log
